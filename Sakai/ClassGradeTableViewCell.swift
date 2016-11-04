228f3b610f006e165b9221ddabdd923788953f61
Katie Kuenster <katiekuenster@Katie-Kuensters-MacBook-Air.local>
2016-11-03 21:39:22 -0400

    added gradebook

---
+++ Sakai/ClassGradeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassGradeTableViewCell.swift
+//  Sakai
+//
+//  Created by Katie Kuenster on 11/3/16.
+//  Copyright © 2016 HCI2016. All rights reserved.
+//
+
+import UIKit
+
+class ClassGradeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
