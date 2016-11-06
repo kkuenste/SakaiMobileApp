08fdca28e6b259b92106830ba3829032ae0e89c3
Katie Kuenster <katiekuenster@katiekustersair.dhcp.nd.edu>
2016-11-06 15:49:31 -0500

    grades tableview added

--- Sakai/AppDelegate.swift
+++ Sakai/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "TopBarFinal"), for: .default)
+        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "TopBarFinal2"), for: .default)
         UINavigationBar.appearance().tintColor = UIColor.white
         UIApplication.shared.statusBarStyle = .lightContent
 
