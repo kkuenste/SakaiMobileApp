4374739924b4ed010253b5cbf0652f2e1f4eaf65
Katie Kuenster <katiekuenster@katiekustersair.dhcp.nd.edu>
2016-11-30 14:00:30 -0500

    added office hour view controller

---
+++ Sakai/OfficeHourViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OfficeHourViewController.swift
+//  Sakai
+//
+//  Created by Katie Kuenster on 11/30/16.
+//  Copyright © 2016 HCI2016. All rights reserved.
+//
+
+import UIKit
+
+class OfficeHourViewController: UIViewController {
+
+    @IBAction func doneButton(_ sender: Any) {
+       dismiss(animated: true, completion: nil) 
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
